7e73c331a2275d9673c1077851c46d505cb044c8
GitHub Action <action@github.com>
2021-07-28 10:33:50 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/lanhuidong/cleancodeplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.lanhuidong.cleancodeplugin.services
+
+import com.github.lanhuidong.cleancodeplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
